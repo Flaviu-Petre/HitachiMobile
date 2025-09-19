9b6737a728dd5f095bb573fdf2e3ae41bf273872
Flaviu-Petre <flaviu.petre09@gmail.com>
2025-09-19 19:13:08 +0300

    [added] Add CustomerAddressRepository and new query to CustomerRepository

---
+++ HitachiMobile/src/main/java/com/SPRING_REST_CAPSTONE/HitachiMobile/repository/CustomerAddressRepository.java
@@ -0,0 +1,9 @@
+package com.SPRING_REST_CAPSTONE.HitachiMobile.repository;
+
+import com.SPRING_REST_CAPSTONE.HitachiMobile.entity.CustomerAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, Integer> {
+}
--- HitachiMobile/src/main/java/com/SPRING_REST_CAPSTONE/HitachiMobile/repository/CustomerRepository.java
+++ HitachiMobile/src/main/java/com/SPRING_REST_CAPSTONE/HitachiMobile/repository/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.SPRING_REST_CAPSTONE.HitachiMobile.repository;
 
 import com.SPRING_REST_CAPSTONE.HitachiMobile.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
@@ -10,4 +11,7 @@ import java.sql.Date;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Customer findByEmailAddressAndDateOfBirth(String emailAddress, String dateOfBirth);
+
+    @Query("SELECT c FROM Customer c WHERE c.uniqueIdNumber = ?1")
+    Customer findByUniqueIdNumber(Long uniqueIdNumber);
 }
