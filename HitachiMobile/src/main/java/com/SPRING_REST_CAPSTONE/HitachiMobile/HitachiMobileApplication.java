3fc9ebb12eaaf4942333d90af9b3c25f380ef5cb
Flaviu-Petre <flaviu.petre09@gmail.com>
2025-09-19 15:33:19 +0300

    [added] Project files

---
+++ HitachiMobile/src/main/java/com/SPRING_REST_CAPSTONE/HitachiMobile/HitachiMobileApplication.java
@@ -0,0 +1,13 @@
+package com.SPRING_REST_CAPSTONE.HitachiMobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitachiMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitachiMobileApplication.class, args);
+	}
+
+}
---
+++ HitachiMobile/src/test/java/com/SPRING_REST_CAPSTONE/HitachiMobile/HitachiMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SPRING_REST_CAPSTONE.HitachiMobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitachiMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
